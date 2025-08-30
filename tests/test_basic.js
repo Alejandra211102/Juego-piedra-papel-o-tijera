82a80892dd2548daf2c33eca6ad2bcec81238500
Alejandra211102 <asantamariag@libertadores.edu.co>
2025-08-29 19:06:20 -0500

    Create test_basic.js

---
+++ tests/test_basic.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../app'); // ajusta la ruta
+describe('Basic Tests', () => {
+test('should respond to GET /', async () => {
+const response = await request(app).get('/');
+expect(response.statusCode).toBe(200);
+});
+});
+# .github/workflows/ci.yml
+name: Node.js Tests
+on:
+push:
